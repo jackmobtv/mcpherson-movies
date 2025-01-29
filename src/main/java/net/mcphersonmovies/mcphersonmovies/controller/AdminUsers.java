81c82585e6a76f83388c1b96db4c5373c737d6c2
Jacob McPherson <jackmob040@gmail.com>
2025-01-29 13:49:24 -0600

    Week 1 Tues

---
+++ src/main/java/net/mcphersonmovies/mcphersonmovies/controller/AdminUsers.java
@@ -0,0 +1,23 @@
+package net.mcphersonmovies.mcphersonmovies.controller;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import net.mcphersonmovies.mcphersonmovies.model.User;
+import net.mcphersonmovies.mcphersonmovies.model.UserDAO;
+
+import java.io.IOException;
+import java.util.List;
+
+@WebServlet("/users")
+public class AdminUsers extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        List<User> users = UserDAO.getAll();
+        req.setAttribute("users", users);
+        req.setAttribute("pageTitle", "All Users");
+        req.getRequestDispatcher("WEB-INF/movies/admin-users.jsp").forward(req, resp);
+    }
+}
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -9,5 +9,6 @@
 </h1>
 <br/>
 <a href="hello-servlet">Hello Servlet</a>
+<a href="users">What a Thrill</a>
 </body>
 </html>
